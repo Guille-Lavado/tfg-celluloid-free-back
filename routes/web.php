aef9f5a70682b490901bd61bb659e2ba1e451f5a
Guille-Lavado <lavadoguille@gmail.com>
2026-04-01 11:03:40 +0200

    Primeros pasos!!!

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+// Route::get('/', function () {
+//   $visited = DB::select('select * from places where visited = ?', [1]); 
+//   $togo = DB::select('select * from places where visited = ?', [0]);
+
+//   return view('travel_list', [ 'visited' => $visited, 'togo' => $togo ] );
+// });
+
+Route::get('/', function () {
+  return view('welcome', [] );
+});
