16ccb9e2e8d4b47899bd634d63cdf9249b618aa2
Guille-Lavado <lavadoguille@gmail.com>
2026-04-09 23:04:31 +0200

    Creadas rutas de API de director, genero y obra

---
+++ app/Http/Controllers/Api/GeneroController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Genero;
+
+class GeneroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $generos = Genero::query()->orderBy('created_at', 'desc')->get();
+    
+        $res_genero = [];
+        foreach($generos as $genero) {
+            $res_genero[] = [
+                'id' => $genero['id'],
+                'nombre' => $genero['nombre'],
+            ];
+        }
+
+        return response()->json($res_genero, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\DirectorController;
+use App\Http\Controllers\Api\GeneroController;
+use App\Http\Controllers\Api\ObraController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +16,10 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/directores', [DirectorController::class, 'index']);
+Route::get('/generos',    [GeneroController::class, 'index']);
+Route::get('/obras',      [ObraController::class, 'index']);   // ?nombre=&genero=&director=
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
